a74d6582e064601ec53afdb76aef09585a034748
drehtisch <38186302+drehtisch@users.noreply.github.com>
2021-06-12 22:18:59 +0200

    Refactor & CleanUp
    - Introduce .gitignore
    - Remove Binaries / compilation artifacts
    - Rename dumps and http for better understanding
    - Use HttpClient over WebRequest
    - Use Task.Delay over Thread.Sleep
    - Use Async / Await where possible
    - Initial for custom storage base path

--- Dump Downloader/DumpService.cs
+++ Dump Downloader/DumpService.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
-using static Dump_Downloader.Http;
+using System.Threading.Tasks;
 using HtmlAgilityPack;
-using Microsoft.VisualBasic;
 using ShellProgressBar;
 
 namespace Dump_Downloader
 {
-    public class Dumps
+    public class DumpService
     {
         public static void Setup()
         {
@@ -23,12 +22,12 @@ namespace Dump_Downloader
             Directory.CreateDirectory($"{path}\\Share\\Dumps\\nations");
             Directory.CreateDirectory($"{path}\\Share\\Dumps\\regions");
         }
-        
+
         public static (List<string>, List<string>) GetDumpsList(string nationOrRegion)
         {
             // Intro
             Console.WriteLine("Getting list of dumps from the source archive...");
-            
+
             // Load/Get HTML from Archive Website
             string url;
             switch (nationOrRegion)
@@ -44,7 +43,7 @@ namespace Dump_Downloader
             }
             var web = new HtmlWeb();
             var doc = web.Load(url);
-            
+
             // Get dump names and dump url and adds them to lists.
             List<string> names = new List<string>();
             List<string> urls = new List<string>();
@@ -53,7 +52,7 @@ namespace Dump_Downloader
                 names.Add(node.InnerText);
                 urls.Add(node.ChildNodes["a"].Attributes["href"].Value);
             }
-            
+
             // Cleanup dumpNames to the desired format of yyyy-mm-dd.xml.gz
             for (int i = 0; i < names.Count; i++)
             {
@@ -90,26 +89,27 @@ namespace Dump_Downloader
             return (dumpNames, dumpUrls);
         }
 
-        public static void DownloadDumps(List<string> dumpNames, List<string> dumpUrls, string userAgent, string nationOrRegion)
+        public static async Task DownloadDumps(List<string> targetDumpNames, List<string> remoteDumpNames, string userAgent, string nationOrRegion, string storagePath)
         {
             // Shell Progress Bar
-            int totalTicks = dumpUrls.Count();
+            int totalTicks = remoteDumpNames.Count;
+            using var _httpService = new HttpService(userAgent);
+
             var options = new ProgressBarOptions
             {
                 ForegroundColor = ConsoleColor.Yellow,
                 ForegroundColorDone = ConsoleColor.DarkGreen,
                 BackgroundColor = ConsoleColor.DarkGray,
                 BackgroundCharacter = '\u2593',
-                ProgressBarOnBottom = true
-
+                ProgressBarOnBottom = true,
             };
-            using var pbar = new ProgressBar(totalTicks, "Initial message", options);
-            for (int i = 0; i < dumpUrls.Count; i++)
+            using var pbar = new ProgressBar(totalTicks, "Loading dumps", options);
+            for (int i = 0; i < remoteDumpNames.Count; i++)
             {
                 // Safety check before continuing
-                if (dumpNames.Count != dumpUrls.Count)
+                if (targetDumpNames.Count != remoteDumpNames.Count)
                 {
-                    throw new Exception($"ERROR: The list lengths of dumpNames and dumpUrls do not match. Program will refuse to continue.Names: {dumpNames.Count}, Urls: {dumpUrls.Count}");
+                    throw new Exception($"ERROR: The list lengths of dumpNames and dumpUrls do not match. Program will refuse to continue.Names: {targetDumpNames.Count}, Urls: {remoteDumpNames.Count}");
                 }
                 // Set path based on nations or region
                 string path;
@@ -124,14 +124,14 @@ namespace Dump_Downloader
                     default:
                         throw new Exception("ERROR: Parameter did not indicate if they are for \"nation\" or \"region\"");
                 }
-                ApiRequest($"https://nationstates.s3.amazonaws.com/{nationOrRegion}_dump/{dumpUrls[i]}", userAgent, $"{path}\\{dumpNames[i]}");
-                pbar.Tick($"Step {i+1} of {totalTicks}");
+                await _httpService.SendRequest($"https://nationstates.s3.amazonaws.com/{nationOrRegion}_dump/{remoteDumpNames[i]}", Path.Join(storagePath, targetDumpNames[i]));
+                pbar.Tick($"Step {i + 1} of {totalTicks}");
             }
         }
-        
+
         static void GzToXML(string xmlPath, string saveLocation)
         {
-            Stream stream =  File.Open(xmlPath, FileMode.Open);
+            Stream stream = File.Open(xmlPath, FileMode.Open);
             using (var instream = new GZipStream(stream, CompressionMode.Decompress))
             using (var outputStream = new FileStream(saveLocation, FileMode.Append, FileAccess.Write))
                 instream.CopyTo(outputStream);
--- Dump Downloader/Program.cs
+++ Dump Downloader/Program.cs
@@ -1,22 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using static Dump_Downloader.Dumps;
+using System.Threading.Tasks;
+using static Dump_Downloader.DumpService;
 
 namespace Dump_Downloader
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
             Intro();
-            
+
             // Gets list of all current dumps. Item1 = names, Item2 = urls.
             Console.Write("Would you like to backup dumps for nation or region? (N/R): ");
             string nationOrRegion = Console.ReadLine().ToUpper();
-            
+
             Console.Write("Please set a User-Agent: ");
             string uAgent = Console.ReadLine();
-            
+
             Setup();
             (List<string>, List<string>) dumpsList;
             switch (nationOrRegion)
@@ -38,29 +39,31 @@ namespace Dump_Downloader
                     nationOrRegion = "regions";
                     break;
                 default:
-                {
-                    Console.WriteLine(
-                        "\nERROR: Your answer was not one of the following: n, r, nation, or region.");
-                    throw new Exception();
-                }
+                    {
+                        Console.WriteLine(
+                            "\nERROR: Your answer was not one of the following: n, r, nation, or region.");
+                        throw new Exception();
+                    }
             }
-            
+
             // Check for existing dumps and return list of everything to get.
             dumpsList = CheckForExistingDumps(dumpsList.Item1, dumpsList.Item2, nationOrRegion);
-            
+
             // Download Dumps
-            
-            DownloadDumps(dumpsList.Item1, dumpsList.Item2, uAgent, nationOrRegion);
+            var storageBasePath = "";
+            await DownloadDumps(dumpsList.Item1, dumpsList.Item2, uAgent, nationOrRegion, storageBasePath);
         }
 
         static void Intro()
         {
             Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Title = "Dump Downloader by Heaveria";
             Console.WriteLine("Dump Downloader");
             Console.ResetColor();
-            Console.WriteLine("Developed By: Heaveria\n" +
-                              "Archive Sources: https://nationstates.s3.amazonaws.com/regions_dump/index.html\n" +
-                              "                 https://nationstates.s3.amazonaws.com/nations_dump/index.html\n");
+            Console.WriteLine($"Developed By: Heaveria{Environment.NewLine}" +
+                $"Archive Sources: https://nationstates.s3.amazonaws.com/regions_dump/index.html" +
+                $"{Environment.NewLine}" +
+                $"                 https://nationstates.s3.amazonaws.com/nations_dump/index.html\n");
         }
     }
 }
