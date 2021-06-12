153e02382825c42cdbdd093c71585666e91454de
Heaveria <me@heaveria.com>
2021-06-12 13:10:16 +0200

    Initial Commit

---
+++ Dump Downloader/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using static Dump_Downloader.Dumps;
+
+namespace Dump_Downloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Intro();
+            
+            // Gets list of all current dumps. Item1 = names, Item2 = urls.
+            Console.Write("Would you like to backup dumps for nation or region? (N/R): ");
+            string nationOrRegion = Console.ReadLine().ToUpper();
+            
+            Console.Write("Please set a User-Agent: ");
+            string uAgent = Console.ReadLine();
+            
+            Setup();
+            (List<string>, List<string>) dumpsList;
+            switch (nationOrRegion)
+            {
+                case "N":
+                    dumpsList = GetDumpsList("nations");
+                    nationOrRegion = "nations";
+                    break;
+                case "NATION":
+                    dumpsList = GetDumpsList("nations");
+                    nationOrRegion = "nations";
+                    break;
+                case "R":
+                    dumpsList = GetDumpsList("regions");
+                    nationOrRegion = "regions";
+                    break;
+                case "REGION":
+                    dumpsList = GetDumpsList("regions");
+                    nationOrRegion = "regions";
+                    break;
+                default:
+                {
+                    Console.WriteLine(
+                        "\nERROR: Your answer was not one of the following: n, r, nation, or region.");
+                    throw new Exception();
+                }
+            }
+            
+            // Check for existing dumps and return list of everything to get.
+            dumpsList = CheckForExistingDumps(dumpsList.Item1, dumpsList.Item2, nationOrRegion);
+            
+            // Download Dumps
+            
+            DownloadDumps(dumpsList.Item1, dumpsList.Item2, uAgent, nationOrRegion);
+        }
+
+        static void Intro()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Dump Downloader");
+            Console.ResetColor();
+            Console.WriteLine("Developed By: Heaveria\n" +
+                              "Archive Sources: https://nationstates.s3.amazonaws.com/regions_dump/index.html\n" +
+                              "                 https://nationstates.s3.amazonaws.com/nations_dump/index.html\n");
+        }
+    }
+}
---
+++ Dump Downloader/dumps.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using static Dump_Downloader.Http;
+using HtmlAgilityPack;
+using Microsoft.VisualBasic;
+using ShellProgressBar;
+
+namespace Dump_Downloader
+{
+    public class Dumps
+    {
+        public static void Setup()
+        {
+            // Create necessary folders. Nothing happens if they already exist.
+            Console.WriteLine("Getting started...\n" +
+                              "Creating directories...");
+            string path = Directory.GetCurrentDirectory();
+            Directory.CreateDirectory($"{path}\\Share");
+            Directory.CreateDirectory($"{path}\\Share\\Dumps");
+            Directory.CreateDirectory($"{path}\\Share\\Dumps\\nations");
+            Directory.CreateDirectory($"{path}\\Share\\Dumps\\regions");
+        }
+        
+        public static (List<string>, List<string>) GetDumpsList(string nationOrRegion)
+        {
+            // Intro
+            Console.WriteLine("Getting list of dumps from the source archive...");
+            
+            // Load/Get HTML from Archive Website
+            string url;
+            switch (nationOrRegion)
+            {
+                case "nations":
+                    url = "https://nationstates.s3.amazonaws.com/nations_dump/index.html";
+                    break;
+                case "regions":
+                    url = "https://nationstates.s3.amazonaws.com/regions_dump/index.html";
+                    break;
+                default:
+                    throw new Exception();
+            }
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+            
+            // Get dump names and dump url and adds them to lists.
+            List<string> names = new List<string>();
+            List<string> urls = new List<string>();
+            foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//li"))
+            {
+                names.Add(node.InnerText);
+                urls.Add(node.ChildNodes["a"].Attributes["href"].Value);
+            }
+            
+            // Cleanup dumpNames to the desired format of yyyy-mm-dd.xml.gz
+            for (int i = 0; i < names.Count; i++)
+            {
+                names[i] = names[i].Replace($"{nationOrRegion}_", "");
+            }
+
+            return (names, urls);
+        }
+
+        public static (List<string> dumpNames, List<string> dumpUrls) CheckForExistingDumps(List<string> dumpNames, List<string> dumpUrls, string nationOrRegion)
+        {
+            // Intro
+            Console.WriteLine("Checking for existing dump files...");
+            // Creates list and adds all existing files into it.
+            List<string> currentDumps = new List<string>();
+            foreach (var dump in Directory.GetFiles($"{Directory.GetCurrentDirectory()}\\Share\\Dumps\\{nationOrRegion}"))
+            {
+                currentDumps.Add(dump.Split('\\').Last());
+            }
+            // Filters out existing files from the dumpNames & dumpUrls list and returns them.
+            for (int i = 0; i < currentDumps.Count; i++)
+            {
+                string currentFile = currentDumps[i];
+                foreach (var dump in dumpNames.ToList())
+                {
+                    if (dump == currentFile)
+                    {
+                        dumpNames.Remove(dump);
+                        dumpUrls.Remove($"{nationOrRegion}_{dump}");
+                    }
+                }
+                //Console.WriteLine(string.Join(",", dumpUrls));
+            }
+            return (dumpNames, dumpUrls);
+        }
+
+        public static void DownloadDumps(List<string> dumpNames, List<string> dumpUrls, string userAgent, string nationOrRegion)
+        {
+            // Shell Progress Bar
+            int totalTicks = dumpUrls.Count();
+            var options = new ProgressBarOptions
+            {
+                ForegroundColor = ConsoleColor.Yellow,
+                ForegroundColorDone = ConsoleColor.DarkGreen,
+                BackgroundColor = ConsoleColor.DarkGray,
+                BackgroundCharacter = '\u2593',
+                ProgressBarOnBottom = true
+
+            };
+            using var pbar = new ProgressBar(totalTicks, "Initial message", options);
+            for (int i = 0; i < dumpUrls.Count; i++)
+            {
+                // Safety check before continuing
+                if (dumpNames.Count != dumpUrls.Count)
+                {
+                    throw new Exception($"ERROR: The list lengths of dumpNames and dumpUrls do not match. Program will refuse to continue.Names: {dumpNames.Count}, Urls: {dumpUrls.Count}");
+                }
+                // Set path based on nations or region
+                string path;
+                switch (nationOrRegion)
+                {
+                    case "nations":
+                        path = $"{Directory.GetCurrentDirectory()}\\Share\\Dumps\\nations";
+                        break;
+                    case "regions":
+                        path = $"{Directory.GetCurrentDirectory()}\\Share\\Dumps\\regions";
+                        break;
+                    default:
+                        throw new Exception("ERROR: Parameter did not indicate if they are for \"nation\" or \"region\"");
+                }
+                ApiRequest($"https://nationstates.s3.amazonaws.com/{nationOrRegion}_dump/{dumpUrls[i]}", userAgent, $"{path}\\{dumpNames[i]}");
+                pbar.Tick($"Step {i+1} of {totalTicks}");
+            }
+        }
+        
+        static void GzToXML(string xmlPath, string saveLocation)
+        {
+            Stream stream =  File.Open(xmlPath, FileMode.Open);
+            using (var instream = new GZipStream(stream, CompressionMode.Decompress))
+            using (var outputStream = new FileStream(saveLocation, FileMode.Append, FileAccess.Write))
+                instream.CopyTo(outputStream);
+        }
+    }
+}
